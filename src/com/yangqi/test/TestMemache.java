2a13b4f0aa08e35dedb51d4f72f15a7541703d7b
yangqi <yangqichris@gmail.com>
2012-08-03 00:17:06 +0800

    fix classpath

--- src/com/yangqi/test/TestMemache.java
+++ src/com/yangqi/test/TestMemache.java
@@ -1,9 +1,7 @@
 /*
- * Copyright 1999-2011 Alibaba.com All right reserved. This software is the
- * confidential and proprietary information of Alibaba.com ("Confidential
- * Information"). You shall not disclose such Confidential Information and shall
- * use it only in accordance with the terms of the license agreement you entered
- * into with Alibaba.com.
+ * Copyright 1999-2011 Alibaba.com All right reserved. This software is the confidential and proprietary information of
+ * Alibaba.com ("Confidential Information"). You shall not disclose such Confidential Information and shall use it only
+ * in accordance with the terms of the license agreement you entered into with Alibaba.com.
  */
 package com.yangqi.test;
 
@@ -12,6 +10,7 @@ import com.meetup.memcached.SockIOPool;
 
 /**
  * this code is written by yangqi
+ * 
  * @author yangqi 2012-8-2 下午01:41:56
  */
 public class TestMemache {
@@ -32,13 +31,18 @@ public class TestMemache {
         pool.initialize();
         /* 建立MemcachedClient实例 */
         MemcachedClient memCachedClient = new MemcachedClient();
+        long start = System.currentTimeMillis();
         for (int i = 0; i < 100; i++) {
             /* 将对象加入到memcached缓存 */
+
             boolean success = memCachedClient.set("" + i, "Hello!Hello!Hello!Hello!");
+            long setEnd = System.currentTimeMillis();
             /* 从memcached缓存中按key值取对象 */
             String result = (String) memCachedClient.get("" + i);
-            System.out.println(String.format("set( %d ): %s", i, success));
-            System.out.println(String.format("get( %d ): %s", i, result));
+            long getEnd = System.currentTimeMillis();
+
+            System.out.println("SET USE " + (setEnd - start) + " ,GET USE " + (getEnd - setEnd));
+
         }
     }
 
